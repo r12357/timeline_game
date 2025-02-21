e847c717c96843d12e660b058e15786d00e06ed1
r12357 <dxdy9955@gmail.com>
2025-02-21 09:48:18 +0900

    getCardを追加

---
+++ src/app/api/getCard/route.ts
@@ -0,0 +1,21 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const cards = await prisma.$queryRaw<
+      {
+        id: string;
+        text: string;
+        year: number;
+      }[]
+    >`SELECT * FROM "Card" ORDER BY RANDOM() LIMIT 5;`;
+
+    return NextResponse.json(cards);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "エラーが発生しました" },
+      { status: 500 }
+    );
+  }
+};
