3345561a244d16911f61741364a4b122bdd1d9de
r12357 <dxdy9955@gmail.com>
2025-02-21 23:49:54 +0900

    キャッシュの削除

--- src/app/api/getCard/route.ts
+++ src/app/api/getCard/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
+export const revalidata = 0;
+
 export const GET = async (req: NextRequest) => {
   try {
     const cards = await prisma.$queryRaw<
